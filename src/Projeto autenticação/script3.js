32771c0ffc6c9fe22d1469f92bb24c1d04136196
Joao-Paulo-Faria <jp_faria@id.uff.br>
2022-11-17 20:30:55 -0300

    atualizacao
    atualizacao

--- src/Projeto autenticação/script3.js
+++ src/Projeto autenticação/script3.js
@@ -1,15 +1,15 @@
-if(!localStorage.getItem('usuario')){
-    alert('Você precia está logado para acessar essa página!')
-    location.href='http://127.0.0.1:5500/index.html'
-}
+//if(!localStorage.getItem('usuario')){
+    //alert('Você precia está logado para acessar essa página!')
+    ////location.href='http://127.0.0.1:5500/index.html'
+////}
 
-let userlogado = JSON.parse(localStorage.getItem('usuario'))
-let logado = document.querySelector('#logado')
-logado.innerHTML =`Olá ${userlogado.nome}`
+//let userlogado = JSON.parse(localStorage.getItem('usuario'))
+//let logado = document.querySelector('#logado')
+//logado.innerHTML =`Olá ${userlogado.nome}`
 
 
-function sair(){
-    localStorage.removeItem('usuario')
+//function sair(){
+    //localStorage.removeItem('usuario')
     
-    location.href='http://127.0.0.1:5500/index.html'
-}
+    //location.href='http://127.0.0.1:5500/index.html'
+///}
