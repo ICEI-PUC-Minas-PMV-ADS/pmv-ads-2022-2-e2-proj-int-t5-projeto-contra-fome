6126323397d6c14c49111dc20505599ca69bc213
Joao-Paulo-Faria <jp_faria@id.uff.br>
2022-11-17 22:27:04 -0300

    Botão
    Botão

--- src/1711/Projeto autenticação2/script3.js
+++ src/1711/Projeto autenticação2/script3.js
@@ -1,88 +1,94 @@
-// if(!localStorage.getItem('usuario')){
-//     alert('Você precia está logado para acessar essa página!')
-//     location.href='Login.html'
-// // }
- const resp =document.getElementById("container")
-
-
-
+ if(!localStorage.getItem('usuario')){
+     alert('Você precia está logado para acessar essa página!')
+     location.href='Login.html'
+}
 let userlogado = ""
+function lerBD(){
+    
     if (localStorage.getItem("usuarioLogado") === null) {
         console.log('teste')
         userlogado = JSON.parse(localStorage.getItem('usuario'))
     }
     else{
         userlogado = JSON.parse(localStorage.getItem('usuarioLogado'))
-     }   
-// let logado = document.querySelector('#logado')
-// let campanha = document.querySelector('#campanha')
-// logado.innerHTML =`Olá ${userlogado.razaoSocial}`
-// let resposta =""
-// let lista = [];
-//const button = document.createElement('button')
+    }
+    let logado = document.querySelector('#logado')
+    logado.innerHTML =`Olá ${userlogado.razaoSocial}`
+    desenhar()
+}
+
+
 
 // lista = resposta.split(".");
-// lista.forEach(element => {
-//     let Ong = (userlogado.cadastroCampanhas);
-//     for (let i = 0; i < Ong.length; i++) {
-//         let Ong2 = (userlogado.cadastroCampanhas[i]);
+//  lista.forEach(element => {
+//      let Ong = (userlogado.cadastroCampanhas);
+//      for (let i = 0; i < Ong.length; i++) {
+//          let Ong2 = (userlogado.cadastroCampanhas[i]);
         
-//         //let ong22 = (userlogado.idCampanha[i]);
+//          let ong22 = (userlogado.idCampanha[i]);
 //         resposta = resposta + Ong2.descricaoDaCampanha + "." + '\n'
         
-//         //resposta = resposta + Ong2.descricaoDaCampanha + document.createElement(`button onclick= teste(${ong22})`) + "\n"
-//     }
+//          resposta = resposta + Ong2.descricaoDaCampanha + document.createElement(`button onclick= teste(${ong22})`) + "\n"
+//      }
     
-// //     resp.innerText = resposta
+//      resp.innerText = resposta
    
    
 // });
 
 
+// const KYE_BD = '@usuariosestudo'
 
 
+// var listaRegistros = {
+//     ultimoIdGerado:0,
+//     usuarios:[
+//         {id:1,nome:'Cassio',fone:'81 9000-0000'},
+//         {id:2,nome:'Cassio',fone:'81 9000-0000'},
+//         {id:3,nome:'Cassio',fone:'81 9000-0000'}
+//     ]
 
-var listaRegistros = {
-    ultimoIdGerado:0,
-    usuarios:[
-        {id:1,nome:'Cassio',fone:'81 9000-0000'},
-        {id:2,nome:'Cassio',fone:'81 9000-0000'},
-        {id:3,nome:'Cassio',fone:'81 9000-0000'}
-    ]
+// }
+//function gravarBD(){
+    //localStorage.setItem(KYE_BD,JSON.stringify(listaRegistros))
+//
 
-}
+
 function desenhar(){
    
     const tbody = document.getElementById('listaRegistrosBody')
     
     if(tbody){
-        tbody.innerHTML = listaRegistros.usuarios.map(usuario =>{
+        tbody.innerHTML = userlogado.cadastroCampanhas.map(usuario =>{
 
             return `<tr>
-                    <td>${usuario.id}</td>
-                    <td>${usuario.nome}</td>
-                    <td>${usuario.fone}</td>
+                    <td>${usuario.descricaoDaCampanha}</td>
+                    <td>
+                            <button  onclick="gerenci()">Editar</button>
+                            <button class='vermelho'>Deletar</button>
+                    </td>
                 </tr>`
 
         } ).join('')
     }
-}
-function insert(nome,fone){
-    const id = listaRegistros.ultimoIdGerado + 1;
-    listaRegistros.usuarios.push({
-        id,nome,fone
-    })
-    desenhar()
-    visualizar('lista')
-
-}
-function edit(id,nome,fone){
-
+} 
+function gerenci(){
+ 
+    location.href='EditarCampanha.html'
 }
 
-function deleteUsuario(id){
 
-}
+// function insert(nome,fone){
+//     const id = listaRegistros.ultimoIdGerado + 1;
+//     listaRegistros.usuarios.push({
+//         id,nome,fone
+//     })
+//     gravarBD()
+//     desenhar()
+//     visualizar('lista')
+
+// }
+
     
 function sair(){
     localStorage.removeItem('usuario')
@@ -90,38 +96,39 @@ function sair(){
     
     location.href='Login.html'
 }
-
-function visualizar(pagina){
-    document.body.setAttribute('page',pagina)
-    if(pagina ==='cadastro'){
-        document.getElementById('nome').focus()
-    }
-    
-}
-
-function submeter(e){
-    e.preventDefault()
-    const data ={
-        id: document.getElementById('id').value,
-        nome: document.getElementById('nome').value,
-        fone: document.getElementById('fone').value,
-    }
-    if(data.id){
-        edit(...data)
-    }else{
-        insert(data.nome, data.fone)
-    }
-
-}
-
-
-
 window.addEventListener('load', () => {
   
-    desenhar()
-    document.getElementById('CadastroRegistro').addEventListener('submit', submeter)
+    lerBD()
+    //document.getElementById('CadastroRegistro').addEventListener('submit', submeter)
 })
-//async function teste(id){
-  //const response = http.get(`api/CadastroCampanhas${id}`)
-//}
+
+// function visualizar(pagina){
+//     document.body.setAttribute('page',pagina)
+//     if(pagina ==='cadastro'){
+//         document.getElementById('nome').focus()
+//     }
+    
+// }
+
+// function submeter(e){
+//     e.preventDefault()
+//     const data ={
+//         id: document.getElementById('id').value,
+//         nome: document.getElementById('nome').value,
+//         fone: document.getElementById('fone').value,
+//     }
+//     if(data.id){
+//         edit(...data)
+//     }else{
+//         insert(data.nome, data.fone)
+//     }
+
+// }
+
+
+
+
+// async function teste(id){
+//   const response = http.get(`api/CadastroCampanhas${id}`)
+// }
 
