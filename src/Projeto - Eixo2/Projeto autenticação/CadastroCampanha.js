9ae6a0b893471fe55b0297afb2b1e97747a4d1a4
Joao-Paulo-Faria <jp_faria@id.uff.br>
2022-12-10 19:20:23 -0300

    melhoria
    melhoria

--- src/Projeto - Eixo2/Projeto autenticação/CadastroCampanha.js
+++ src/Projeto - Eixo2/Projeto autenticação/CadastroCampanha.js
@@ -1,15 +1,15 @@
 
-async function cadastrar(){
- 
-    let nomeDaOng = document.getElementById('NomeDaOng').value || $('NomeDaOng').val();
-    let nomeDaCampanha = document.getElementById('NomeDaCampanha').value || $('NomeDaCampanha').val();
-    let descricaoDaCampanha = document.getElementById('descricaoDaCampanha').value || $('descricaoDaCampanha').val();
-    let telefone = document.getElementById('Telefone').value || $('Telefone').val();
-    let email = document.getElementById('Email').value || $('Email').val();
-    let endereco = document.getElementById('Endereco').value || $('Endereco').val();
-    let cidade = document.getElementById('cidade').value || $('cidade').val();
-    let estado = document.getElementById('Estado').value || $('Estado').val();
-    let pais = document.getElementById('Pais').value || $('Pais').val();
+async function cadastrar() {
+
+    let nomeDaOng = document.querySelector("#NomeDaOng").value || $("#NomeDaOng").val();
+    let nomeDaCampanha = document.querySelector("#NomeDaCampanha").value || $("#NomeDaCampanha").val();
+    let descricaoDaCampanha = document.querySelector("#descricaoDaCampanha").value || $("#descricaoDaCampanha").val();
+    let telefone = document.querySelector("#Telefone").value || $("#Telefone").val();
+    let email = document.querySelector("#Email").value || $("#Email").val();
+    let endereco = document.querySelector("#Endereco").value || $("#Endereco").val();
+    let cidade = document.querySelector("#cidade").value || $("#cidade").val();
+    let estado = document.querySelector("#Estado").value || $("#Estado").val();
+    let pais = document.querySelector("#Pais").value || $("#Pais").val();
     // let pix = document.getElementById('Pix').value || $('Pix').val();
     // let cartaoDeCredito = document.getElementById('CartaoDeCredito').value || $('CartaoDeCredito').val();
     // let receberFisico = document.getElementById('ReceberFisico').value || $('ReceberFisico').val();
@@ -18,63 +18,75 @@ async function cadastrar(){
     let pix = false;
     let cartaoDeCredito = false;
     let receberFisico = false;
-    if (document.getElementById('Pix').value != null) {
-       pix = document.getElementById('Pix').value === 'sim' || document.getElementById('Pix').value === 'Sim' ? true : false; 
+    if (document.querySelector("#Pix").value != null) {
+        pix = document.querySelector("#Pix").value === 'sim' || document.querySelector("#Pix").value === 'Sim'|| document.querySelector("#Pix").value === 'SIM' ? true : false;
     };
-    if (document.getElementById('CartaoDeCredito').value != null) {
-      cartaoDeCredito = document.getElementById('CartaoDeCredito').value === 'sim' || document.getElementById('CartaoDeCredito').value === 'Sim' ? true : false; 
-    };
-   
-    if (document.getElementById('ReceberFisico').value != null) {
-      receberFisico = document.getElementById('ReceberFisico').value === 'sim' || document.getElementById('ReceberFisico').value === 'Sim' ? true : false; 
+    if (document.querySelector("#CartaoDeCredito").value != null) {
+        cartaoDeCredito = document.querySelector("#CartaoDeCredito").value === 'sim' || document.querySelector("#CartaoDeCredito").value === 'Sim' || document.querySelector("#Pix").value === 'SIM' ? true : false;
     };
 
-    http.post(`api/CadastroCampanhas`, {
-        nomeDaOng,
-        nomeDaCampanha,
-        descricaoDaCampanha,
-        telefone,
-        email,
-        endereco,
-        cidade,
-        estado,
-        pais,
-        pix,
-        cartaoDeCredito,
-        receberFisico,
-        cadastroCodigo
-    })
+    if (document.querySelector("#ReceberFisico").value != null) {
+        receberFisico = document.querySelector("#ReceberFisico").value === 'sim' || document.querySelector("#ReceberFisico").value === 'Sim' || document.querySelector("#Pix").value === 'SIM' ? true : false;
+    };
+    // alert(nomeDaOng )
+    // alert(nomeDaCampanha)
+    // alert(descricaoDaCampanha)
+    // alert(telefone)
    
-        if(nomeDaOng ==" " || nomeDaCampanha ==" "){
-            msgError.setAttribute('style','display:block')
-            msgError.innerHTML = '<strong>Preencha todos os campos corretamente antes de cadastrar</strong>'
-            msgSuccess.innerHTML = ''
-            msgSuccess.setAttribute('style','display:none')
-            
-        }else{
-            
-            msgSuccess.setAttribute('style','display:block')
-            msgSuccess.innerHTML = '<strong>Cadastrando usuário, favor aguardar...</strong>'
-            msgError.setAttribute('style','display:none')
-            msgError.innerHTML = ''
-            setTimeout(()=>{
-                buscaCampanhasLogado()
-               // window.location.href='Gerenciar.html'
-            }, 3000)
-            
-        }
+    if (nomeDaOng == "" || nomeDaCampanha== "" || descricaoDaCampanha==""  || telefone=="" || email=="" || endereco=="" || cidade=="" || estado=="" || pais=="") {
+
         
+        // msgError.setAttribute('style', 'display:block')
+        // msgError.innerHTML = '<strong>Preencha todos os campos corretamente antes de cadastrar</strong>'
+        // msgSuccess.innerHTML = ''
+        // msgSuccess.setAttribute('style', 'display:none')
+        
+        return  alert("Existe campo vazio!")
+    }else {
+
+        msgSuccess.setAttribute('style', 'display:block')
+        msgSuccess.innerHTML = '<strong>Cadastrando usuário, favor aguardar...</strong>'
+        msgError.setAttribute('style', 'display:none')
+        msgError.innerHTML = ''
+
+        http.post(`api/CadastroCampanhas`, {
+            nomeDaOng,
+            nomeDaCampanha,
+            descricaoDaCampanha,
+            telefone,
+            email,
+            endereco,
+            cidade,
+            estado,
+            pais,
+            pix,
+            cartaoDeCredito,
+            receberFisico,
+            cadastroCodigo
+        })
+
+       
+        setTimeout(() => {
+            buscaCampanhasLogado()
+            // window.location.href='Gerenciar.html'
+        }, 3000)
+
+    }
+
+   
+
+
 }
 
-async function buscaCampanhasLogado(){
+async function buscaCampanhasLogado() {
     let userlogado = JSON.parse(localStorage.getItem('usuario'))
     const response = await http.get(`api/Cadastro/${userlogado.codigo}`)
-        localStorage.setItem('usuarioLogado', JSON.stringify(response.data))
-        window.location.href='Gerenciar.html'
+    localStorage.setItem('usuarioLogado', JSON.stringify(response.data))
+    window.location.href = 'Gerenciar.html'
 }
 
-function sair(){
-    location.href='Gerenciar.html'
+function sair() {
+    location.href = 'Gerenciar.html'
 }
 
 
